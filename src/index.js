6412fd88532f3b6d5ea2b142f495a07271c9a685
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-11-25 15:18:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const { Util } = require('discord.js')
 module.exports = {
     execute: function (message, args) {
         let cleanMessage = Util.cleanContent(args.join(" "))
-        message.channel.send(cleanMessage + " " + message.author.username) 
+        message.channel.send(cleanMessage + "\n-" + message.author.username) 
     }
 }
